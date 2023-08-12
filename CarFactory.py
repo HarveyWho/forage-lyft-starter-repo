b16a0ac2d017c4639700a4290962e12aaf7d8f07
HarveyWho <haoxuanhong5@gmail.com>
2023-08-12 09:40:46 +0800

    Added CarFactory interface

---
+++ CarFactory.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+from datetime import date
+from car import Car
+
+class CarFactory(ABC):
+
+    @abstractmethod
+    def create_calliope(self, current_date: date, last_service_date: date, 
+                        current_mileage: int, last_service_mileage: int) -> Car:
+        pass
+
+    @abstractmethod
+    def create_glissade(self, current_date: date, last_service_date: date, 
+                        current_mileage: int, last_service_mileage: int) -> Car:
+        pass
+
+    @abstractmethod
+    def create_palindrome(self, current_date: date, last_service_date: date, 
+                           warning_light_on: bool) -> Car:
+        pass
+
+    @abstractmethod
+    def create_rorschach(self, current_date: date, last_service_date: date, 
+                         current_mileage: int, last_service_mileage: int) -> Car:
+        pass
+
+    @abstractmethod
+    def create_thovex(self, current_date: date, last_service_date: date, 
+                      current_mileage: int, last_service_mileage: int) -> Car:
+        pass
