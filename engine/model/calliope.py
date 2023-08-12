9ca1089698090accb2bdef3e37cf0e8382fdfbc1
HarveyWho <haoxuanhong5@gmail.com>
2023-08-12 17:01:23 +0800

    moved battery files to correct places, fixed Calli

--- engine/model/calliope.py
+++ engine/model/calliope.py
@@ -1,12 +1,8 @@
 from datetime import datetime
 
 from engine.capulet_engine import CapuletEngine
+from battery import SpindlerBattery
 
-
-class Calliope(CapuletEngine):
+class Calliope(CapuletEngine, SpindlerBattery):
     def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
-        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
-            return True
-        else:
-            return False
+        return (CapuletEngine.needs_service or SpindlerBattery.SpindlerBattery.needs_service)
